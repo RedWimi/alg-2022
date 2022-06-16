1e8e4a01f0d3e6914ae73f23413f28d29c5ad34a
RedWimi <99308580+RedWimi@users.noreply.github.com>
2022-06-16 13:30:10 +0300

    Винарное дерево

---
+++ BinaryTree/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+
+        tree.addNodeToLeft(tree.root());
+        tree.addNodeToRight(tree.root());
+
+        tree.addNodeToLeft(tree.root().left());
+        tree.addNodeToRight(tree.root().right());
+
+        tree.addNodeToRight(tree.root().left());
+        tree.addNodeToLeft(tree.root().right());
+
+        tree.addNodeToRight(tree.root().right().right());
+
+        System.out.println("Высота дерева = " + tree.height());
+    }
+}
